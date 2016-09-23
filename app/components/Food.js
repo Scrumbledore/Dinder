f0936452c73ef56c98b4b998619a203108ef590a
Ryan Busby <BusbyActual@gmail.com>
2016-09-23 16:48:01 -0700

    Working image loading and icons now

--- app/components/Food.js
+++ app/components/Food.js
@@ -2,49 +2,208 @@ import React, { Component } from 'react';
 import Button from 'react-native-button';
 import {
   StyleSheet,
+  Image,
   Text,
+  TouchableOpacity,
   View
 } from 'react-native';
+import SwipeCards from 'react-native-swipe-cards';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import Iconz from 'react-native-vector-icons/Ionicons';
+
+
+var image1 = require('../images/image1.jpg')
+var image2 = require('../images/image2.jpg')
+var image3 = require('../images/image3.jpg')
+var image4 = require('../images/image4.jpg')
+var image5 = require('../images/image5.jpg')
+var image6 = require('../images/image6.jpg')
+
+const Cards = [{
+  "id": 1,
+  "first_name": "Yummy yummy resturant",
+  "age": 21,
+  "friends": 9,
+  "interests": 38,
+  "image": image1
+}, {
+  "id": 2,
+  "first_name": "The food place",
+  "age": 27,
+  "friends": 16,
+  "interests": 49,
+  "image": image2
+}, {
+  "id": 3,
+  "first_name": "Cafe los potatoes",
+  "age": 29,
+  "friends": 2,
+  "interests": 39,
+  "image": image3
+}, {
+  "id": 4,
+  "first_name": "Vegetables",
+  "age": 20,
+  "friends": 18,
+  "interests": 50,
+  "image": image4
+}, {
+  "id": 5,
+  "first_name": "Food inc",
+  "age": 28,
+  "friends": 2,
+  "interests": 13,
+  "image": image5
+}, {
+  "id": 6,
+  "first_name": "Food food food",
+  "age": 24,
+  "friends": 12,
+  "interests": 44,
+  "image": image6
+}]
 
 export default class Food extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      message: 'Loading...'
+      message: 'Loading...',
+      cards: Cards
     };
   }
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text style={styles.welcome}>
+//           Welcome to the Food page! abcdefg
+//         </Text>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center'
+//   }
+// });
+ Card(x){
+    return (
+      <View style={styles.card}>
+        <Image source ={x.image} resizeMode="contain" style ={{width:350, height:350}} />
+        <View style={{width:350, height:70, flexDirection:'row', alignItems:'center', justifyContent:'space-between'}}>
+        <View style={{flexDirection:'row', margin:15, marginTop:25,}} >
+        <Text style={{fontSize:20, fontWeight:'300', color:'#444'}}>{x.first_name}, </Text>
+        </View>
+        <View style={{flexDirection:'row'}}>
+        <View style={{padding:13,  borderLeftWidth:1,borderColor:'#e3e3e3', alignItems:'center', justifyContent:'space-between'}}><Text>Icon2</Text><Text style={{fontSize:16, fontWeight:'200', color:'#555'}}></Text></View>
+        <View style={{padding:13, borderLeftWidth:1,borderColor:'#e3e3e3', alignItems:'center', justifyContent:'space-between'}}><Text>Icon1</Text><Text style={{fontSize:16, fontWeight:'200', color:'#555'}}></Text></View>
+        </View>
+        </View>
+      </View>
+    )
+  }
+    handleYup (card) {
+    console.log(`Yup for ${card.text}`)
+  }
+
+  handleNope (card) {
+    console.log(`Nope for ${card.text}`)
+  }
+  noMore(){
+    return (
+      <View style={styles.card} >
+        <Text>No More Cards</Text>
+      </View>
+    )
+  }
+
+  yup(){
+    console.log(this.refs['swiper'])
+this.refs['swiper']._goToNextCard()  }
+
+nope(){
+    console.log(this.refs['swiper'])
+this.refs['swiper']._goToNextCard()  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to the Food page!
-        </Text>
-      </View>
-    );
+      <SwipeCards
+        ref = {'swiper'}
+        cards={this.state.cards}
+        containerStyle = {{  backgroundColor: '#f7f7f7', alignItems:'center', margin:20}}
+        renderCard={(cardData) => this.Card(cardData)}
+        renderNoMoreCards={() => this.noMore()}
+        handleYup={this.handleYup}
+        handleNope={this.handleNope} />
+        <View style={{flexDirection:'row', alignItems:'center', justifyContent:'center'}}>
+        <TouchableOpacity style = {styles.buttons} >
+        <Iconz name='ios-close' size={45} color="#888" style={{}} />
+        </TouchableOpacity>
+        <TouchableOpacity style = {styles.buttonSmall}>
+
+        </TouchableOpacity>
+        <TouchableOpacity style = {styles.buttons} >
+
+        </TouchableOpacity>
+        </View>
+        </View>
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+
+    backgroundColor: '#f7f7f7',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  buttons:{
+    width:80,
+    height:80,
+    borderWidth:10,
+    borderColor:'#e7e7e7',
+    justifyContent:'center',
+    alignItems:'center',
+    borderRadius:40
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  buttonSmall:{
+    width:50,
+    height:50,
+    borderWidth:10,
+    borderColor:'#e7e7e7',
+    justifyContent:'center',
+    alignItems:'center',
+    borderRadius:25
   },
-  container: {
+   card: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    alignSelf:'center',
+    borderWidth:2,
+    borderColor:'#e3e3e3',
+    width: 350,
+    height: 420,
   }
+
 });
