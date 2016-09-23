52cbf52865ebe29258c83ca08b40a8723ba5d909
Graham Wimbrow <graham.wimbrow@gmail.com>
2016-09-23 16:07:27 -0400

    fix indentation

--- spec/server/basic.js
+++ spec/server/basic.js
@@ -7,8 +7,8 @@ var apiRoot;
 describe('the API server', function () {
   before(function () {
     apiRoot = process.env.NODE_ENV !== 'production'
-                ? 'http://localhost'
-                : config.apiRoot;
+            ? 'http://localhost'
+            : config.apiRoot;
   });
   it('should respond to GET requests', function (done) {
     request(apiRoot + ':' + config.port + '/', function (err, res, body) {
