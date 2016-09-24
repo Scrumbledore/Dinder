c9fac27b0305ca681f48f9dc6902913dd02663c7
Ryan Busby <BusbyActual@gmail.com>
2016-09-23 17:11:53 -0700

    Changed colors and removed unnecessary features.

--- app/components/Food.js
+++ app/components/Food.js
@@ -21,45 +21,27 @@ var image6 = require('../images/image6.jpg')
 
 const Cards = [{
   "id": 1,
-  "first_name": "Yummy yummy resturant",
-  "age": 21,
-  "friends": 9,
-  "interests": 38,
+  "first_name": "Pikachu salad",
   "image": image1
 }, {
   "id": 2,
-  "first_name": "The food place",
-  "age": 27,
-  "friends": 16,
-  "interests": 49,
+  "first_name": "Les Vegetables",
   "image": image2
 }, {
   "id": 3,
-  "first_name": "Cafe los potatoes",
-  "age": 29,
-  "friends": 2,
-  "interests": 39,
+  "first_name": "Bonne Tuna pocket",
   "image": image3
 }, {
   "id": 4,
-  "first_name": "Vegetables",
-  "age": 20,
-  "friends": 18,
-  "interests": 50,
+  "first_name": "Chicken sticks",
   "image": image4
 }, {
   "id": 5,
-  "first_name": "Food inc",
-  "age": 28,
-  "friends": 2,
-  "interests": 13,
+  "first_name": "Interstellar doughnuts",
   "image": image5
 }, {
   "id": 6,
-  "first_name": "Food food food",
-  "age": 24,
-  "friends": 12,
-  "interests": 44,
+  "first_name": "Orange chicken",
   "image": image6
 }]
 
@@ -72,58 +54,21 @@ export default class Food extends Component {
       cards: Cards
     };
   }
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text style={styles.welcome}>
-//           Welcome to the Food page! abcdefg
-//         </Text>
-//       </View>
-//     );
-//   }
-// }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 5,
-//   },
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center'
-//   }
-// });
  Card(x){
     return (
       <View style={styles.card}>
         <Image source ={x.image} resizeMode="contain" style ={{width:350, height:350}} />
         <View style={{width:350, height:70, flexDirection:'row', alignItems:'center', justifyContent:'space-between'}}>
-        <View style={{flexDirection:'row', margin:15, marginTop:25,}} >
-        <Text style={{fontSize:20, fontWeight:'300', color:'#444'}}>{x.first_name}, </Text>
-        </View>
-        <View style={{flexDirection:'row'}}>
-        <View style={{padding:13,  borderLeftWidth:1,borderColor:'#e3e3e3', alignItems:'center', justifyContent:'space-between'}}><Text>Icon2</Text><Text style={{fontSize:16, fontWeight:'200', color:'#555'}}></Text></View>
-        <View style={{padding:13, borderLeftWidth:1,borderColor:'#e3e3e3', alignItems:'center', justifyContent:'space-between'}}><Text>Icon1</Text><Text style={{fontSize:16, fontWeight:'200', color:'#555'}}></Text></View>
+        <View style={{flexDirection:'row', margin:15, marginTop:25, }} >
+        <Text style={{fontSize:30, fontWeight:'400', textAlign:'center', color:'#444'}}>{x.first_name}</Text>
         </View>
         </View>
       </View>
     )
   }
     handleYup (card) {
-    console.log(`Yup for ${card.text}`)
+    console.log(`Yes for ${card.text}`)
   }
 
   handleNope (card) {
@@ -137,13 +82,16 @@ export default class Food extends Component {
     )
   }
 
-  yup(){
+  yes(){
     console.log(this.refs['swiper'])
 this.refs['swiper']._goToNextCard()  }
 
 nope(){
     console.log(this.refs['swiper'])
 this.refs['swiper']._goToNextCard()  }
+fav(){
+    console.log('favorited', this.refs['swiper'])
+  }
 
   render() {
     return (
@@ -157,14 +105,14 @@ this.refs['swiper']._goToNextCard()  }
         handleYup={this.handleYup}
         handleNope={this.handleNope} />
         <View style={{flexDirection:'row', alignItems:'center', justifyContent:'center'}}>
-        <TouchableOpacity style = {styles.buttons} >
-        <Iconz name='ios-close' size={45} color="#888" style={{}} />
+        <TouchableOpacity style = {styles.buttons} onPress = {() => this.nope()}>
+        <Iconz name='ios-close' size={45} color="#111111" style={{}} />
         </TouchableOpacity>
-        <TouchableOpacity style = {styles.buttonSmall}>
-
+        <TouchableOpacity style = {styles.buttons} onPress = {() => this.yes()}>
+        <Iconz name='ios-heart-outline' size={36} color="#FF4136" style={{marginTop:5}} />
         </TouchableOpacity>
-        <TouchableOpacity style = {styles.buttons} >
-
+          <TouchableOpacity style = {styles.buttons} onPress = {() => this.fav()}>
+        <Iconz name='ios-star' size={36} color="#FFDC00" style={{marginTop:5}} />
         </TouchableOpacity>
         </View>
         </View>
