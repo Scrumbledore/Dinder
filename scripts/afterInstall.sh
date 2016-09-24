1817aacb3842ca62532a97e48ec6f88d6670c4de
dan-h-ch <chang.daniel@gmail.com>
2016-09-24 16:42:00 -0700

    (fix) updated yml, script

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -3,4 +3,5 @@
 echo After Install
 pm2 stop all
 npm install
-pm2 start /opt/codedeploy-agent/app/dinder/server/index.js
+# pm2 start /opt/codedeploy-agent/app/dinder/server/index.js
+pm2 restart all
