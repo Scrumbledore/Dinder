0178aca91a2f11c729c24070f18bf197ff269cbd
dan-h-ch <chang.daniel@gmail.com>
2016-09-24 17:00:38 -0700

    (fix) restart script fix

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # After install
 echo After Install
+pwd
 pm2 stop all
 npm install
-# pm2 start /opt/codedeploy-agent/app/dinder/server/index.js
-pm2 restart all
+pm2 start /opt/codedeploy-agent/app/dinder/server/index.js
+# pm2 restart all
