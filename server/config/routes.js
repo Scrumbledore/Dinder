dab023cb87553646f7dbe49a405af0a9495be53f
Daniel Chang <chang.daniel@gmail.com>
2016-09-26 07:05:13 -0700

    (feat) confirmed backend route stubs (#56)
    * (feat) backend - photoHandler & userHandler stubs
    
    * (feat) authHandler stubs in place

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,4 +1,6 @@
 var userHandler = require('../handlers/userHandler.js');
+var photoHandler = require('../handlers/photoHandler.js');
+var authHandler = require('../handlers/authHandler.js');
 
 module.exports = function (app, express) {
   // this is root entry - only used for verify server, not in production ap
@@ -16,34 +18,22 @@ module.exports = function (app, express) {
   app.post('/api/favorites/:userid/:pictureid', userHandler.saveFavorite);
 
   // for getting recommendations for a user at a given loc
-  app.get('/api/recommendations/:userid/:loc', function (req, res) {
-    // sometihng here
-  });
+  app.get('/api/recommendations/:userid/:loc', userHandler.getRecommendations);
 
   // for getting pictures of food for user to swipe on
-  app.get('/api/food/:userid/:loc', function (req, res) {
-    // something here
-  });
+  app.get('/api/photo/:userid/:loc', photoHandler.getPhotos);
 
-  // vote yet on a picture (since it's post we can technical pass in body if we want)
-  app.post('/api/yes/:userid/:pictureid', function (req, res) {
-    // something here
-  });
+  // vote yets on a picture (since it's post we can technical pass in body if we want)
+  app.post('/api/yes/:userid/:pictureid', photoHandler.upVote);
 
   // vote no on a picture (since it's post we can technical pass in body if we want)
-  app.post('/api/no/:userid/:pictureid', function (req, res) {
-    // something here
-  });
+  app.post('/api/no/:userid/:pictureid', photoHandler.downVote);
 
   // signin
-  app.post('/api/signin', function (req, res) {
-    // something here
-  });
+  app.post('/api/signin', authHandler.signIn);
 
   // signup
-  app.post('/api/signup', function (req, res) {
-    // something here
-  });
+  app.post('/api/signup', authHandler.signUp);
 
   // more routes here
 };
--- server/handlers/userHandler.js
+++ server/handlers/userHandler.js
@@ -6,14 +6,25 @@ module.exports = {
   // unless explicitly implied no call require data to be in a specific format outside of URL
 
   getFavorites: function (req, res) {
-    var userId = req.params.id;
+    var userId = req.params.userid;
     // something here
+    console.log('getFavorites for', userId);
   },
 
   saveFavorite: function (req, res) {
-    var userId = req.params.id;
+    var userId = req.params.userid;
     var pictureId = req.params.pictureid;
     // something here
-  }
+    console.log('save', pictureId, 'for', userId);
+  },
+
+  getRecommendations: function (req, res) {
+    var userId = req.params.userid;
+    var location = req.params.loc;
+    // somthing here
+    console.log('getRecommendations for', userId, 'at', location);
+  },
+
+
 
 };
