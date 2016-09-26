f7fbcb9981332ed4a050f40edc094709507d240e
dan-h-ch <chang.daniel@gmail.com>
2016-09-26 14:18:32 -0700

    (feat) authHandler - signup partial complete

--- server/database/database.js
+++ server/database/database.js
@@ -10,4 +10,13 @@ var sequelize = new Sequelize(config.DB_NAME, config.DB_USERNAME, config.DB_PASS
   }
 });
 
+// var sequelize = new Sequelize(config.DB_NAME, {
+//   host: 'localhost',
+//   port: 5432,
+//   dialect: 'postgres',
+//   dialectOptions: {
+//     ssl: false
+//   }
+// });
+
 module.exports = sequelize;
