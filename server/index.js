d8b980c86cc0753f99a7aed9b19c88900d2c3084
Ryan Busby <BusbyActual@gmail.com>
2016-09-27 15:06:21 -0700

    Removed test data from indexFile

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var config = require('../config.js');
 var connection = require('./database/database.js');
-//var Place = require('./database/models/place.js');
 
 var app = express();
 
@@ -16,26 +15,6 @@ app.set('port', config.port);
 connection.sync().then(function () {
   app.listen(app.get('port'), function() {
     console.log('listening on port ', app.get('port'));
-    // var req = {
-    //   params :{ lat: 37.7845159579632,
-    //   lon: -122.395666837692,
-    //   name: 'Eden Plaza Cafe',
-    //   address: '600 Harrison St',
-    //   city: 'San Francisco',
-    //   state: 'CA',
-    //   url: 'https://s3-media1.fl.yelpcdn.com/bphoto/zvnkqjGhRhw2ntWb4ngpPw/o.jpg' }
-    // }
-    //   Place.create({
-    //   lat: req.params.lat,
-    //   lon: req.params.lon,
-    //   name: req.params.name,
-    //   address: req.params.address,
-    //   city: req.params.city,
-    //   state: req.params.state,
-    //   url: req.params.url
-    // }).then(function(newPlace) {
-    //   console.log(newPlace);
-    // });
   });
 }).catch(function (err) {
   console.log(err);
