4e007fb555c792515be86c0bb9dc9869baf70244
Ryan Busby <BusbyActual@gmail.com>
2016-09-26 20:01:47 -0700

    Exported yelp handlers.  Please enter the commit message for your changes. Lines starting

--- server/config/yelpRouter.js
+++ server/config/yelpRouter.js
@@ -3,6 +3,7 @@ var rp = require('request-promise');
 
 var config = require('../../config.js');
 
+<<<<<<< 8e0fa879c7d8498c58e1b2c6d83c72bee8c08b56
 /*
 Yelp docs if you want to test different queries:
 https://www.yelp.com/developers/documentation/v3/get_started
@@ -34,72 +35,74 @@ var photo = db.define('photo', {
 
 */
 
+=======
+var stuff1 = [];
+var stuff2 = [];
+
+module.exports = {
+>>>>>>> Exported yelp handlers.
 //hardcoding apiKey until better solution / deployment
-var yelpOptions = function (query, branch) {
+  yelpOptions: function (query, branch) {
   //default for testing.
-  query = query || 'search?term=delis&latitude=37.786882&longitude=-122.399972';
-  branch = branch || '';
+    query = query || 'search?term=delis&latitude=37.786882&longitude=-122.399972';
+    branch = branch || '';
 
-  var options = {
-    url: config.yelpRoot + branch + query,
-    headers: {
-      'Authorization': 'Bearer ' + config.yelpKey,
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
-  };
-  console.log(options);
-  return options;
-};
+    var options = {
+      url: config.yelpRoot + branch + query,
+      headers: {
+        'Authorization': 'Bearer ' + config.yelpKey,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    };
+    console.log(options);
+    return options;
+  },
 
-//Once we have all local businesses we have an array of ID's to query the business to get all pictures
-var allImages = function (businessID) {
-  businessID.forEach(function(business) {
-    rp(yelpOptions(business[0], 'businesses/'))
-     .then(function(item) {
-       var info = JSON.parse(item);
-       var newArray = [info.id];
-       if (info.photos.length > 0) {
-         info.photos.forEach(function(picture) {
-           newArray.push({
-             info: '',
-             url: picture
+//Once we have all local businesses we have an array of ID's to query the business to get all pictures Yelp.yelpOptions(business[0], 'businesses/'))
+  allImages: function (businessID, options) {
+    businessID.forEach(function(business) {
+      rp(options)
+       .then(function(item) {
+         var info = JSON.parse(item);
+         var newArray = [info.id];
+         if (info.photos.length > 0) {
+           info.photos.forEach(function(picture) {
+             newArray.push({
+               info: '',
+               url: picture
+             });
            });
-         });
-       }
+         }
 
-       stuff2.push(newArray);
-       console.log(newArray, info.photos);
-     })
-     .then(console.log(stuff1, stuff2));
-  });
+         stuff2.push(newArray);
+         console.log(newArray, info.photos);
+       })
+       .then(console.log(stuff1, stuff2));
+    });
+  },
+
+//Find initial businesses and business pictures. yelpOptions(null, 'businesses/')
+  someImages: function(options) {
+    var test = [];
+    rp(options)
+      .then(function(data) {
+        return JSON.parse(data);
+      })
+      .then(function(data) {
+        data.businesses.forEach(function(business) {
+          test.push([business.id, {
+            lat: business.coordinates.latitude,
+            lon: business.coordinates.longitude,
+            name: business.name,
+            address: business.location.address1,
+            city: business.location.city,
+            state: business.location.state,
+            url: business.image_url
+          }]);
+        });
+        console.log(test);
+      });
+    return test;
+  }
 };
 
-//Find initial businesses and business pictures.
-rp(yelpOptions(null, 'businesses/'))
-  .then(function(data) {
-    var info = JSON.parse(data);
-    info.businesses.forEach(function(business) {
-      stuff1.push([business.id, business.image_url]);
-    });
-    console.log(data, stuff1);
-    return stuff1;
-  })
- .then(function(data) {
-   var info = JSON.parse(data);
-   info.businesses.forEach(function(business) {
-     stuff1.push([business.id, {
-       lat: business.coordinates.latitude,
-       lon: business.coordinates.longitude,
-       name: business.name,
-       address: business.location.address1,
-       city: business.location.city,
-       state: business.location.state,
-       url: business.image_url
-     }]);
-   });
-   console.log(data, stuff1);
-   return stuff1;
- })
- .then(function(items) {
-   allImages(items);
- });
