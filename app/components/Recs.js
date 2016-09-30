f00a887ddae7ce631c3b7215940408f1d6715b8a
dan-h-ch <chang.daniel@gmail.com>
2016-09-29 21:14:59 -0700

    (feat) favorites - working on height resize; recs - pull in geoloc

--- app/components/Recs.js
+++ app/components/Recs.js
@@ -7,15 +7,41 @@ import styles from '../styles/styles.js';
 export default class Recs extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      initialPosition: undefined
+    }
   }
 
-  render() {
+  // render() {
+  //   return (
+  //     <View style={styles.container}>
+  //       <Text style={styles.welcome}>
+  //         Welcome to the Recs Page!
+  //       </Text>
+  //     </View>
+  //   );
+  // }
+
+  componentWillMount() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      var initialPosition = position;
+      this.setState({initialPosition: initialPosition});
+      // alert(JSON.stringify(this.state.initialPosition))
+      // console.log("coords", this.state.initialPosition.coords)
+      // console.log("posit", this.state.initialPosition)
+    },
+    (error) => alert("Please enable location services."),
+    {enableHighAccurracy: true, timeout: 20000, maxinumAge: 1000}
+    )
+  }
+
+  render() {  
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to the Recs Page!
-        </Text>
+        <Text style={styles.welcome}>Recs page</Text>
+        <Text style={styles.welcome}>Long: {this.state.initialPosition ? this.state.initialPosition.coords.longitude : "Please enable location services"}</Text>
+        <Text style={styles.welcome}>Lat: {this.state.initialPosition ? this.state.initialPosition.coords.latitude : "Please enable location services"}</Text>
       </View>
-    );
+    )
   }
 }
