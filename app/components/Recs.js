849c1ac9991f72898facd10c243a3018788182a1
dan-h-ch <chang.daniel@gmail.com>
2016-09-30 16:10:48 -0700

    (feat) recommendations front end - framework in palce

--- app/components/Recs.js
+++ app/components/Recs.js
@@ -1,46 +1,98 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ListView, Image } from 'react-native';
 import { Tabs, Tab, Icon, Button } from 'react-native-elements';
 
 import styles from '../styles/styles.js';
+
+const ds = new ListView.DataSource({
+  rowHasChanged: (r1, r2) => r1 !== r2,
+  sectionHeaderHasChanged: (s1, s2) => s1 !== s2
+});
 
 export default class Recs extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      initialPosition: undefined
+      long: undefined,
+      lat: undefined,
+      fakeData: [{
+        name: 'Molinari Delicatessen',
+        address: '373 Columbus Ave ',
+        city: 'San Francisco',
+        state: 'CA',
+        url: 'https://s3-media3.fl.yelpcdn.com/bphoto/H_vQ3ElMoQ8j1bKidrv_1w/o.jpg'
+      }]
     };
   }
 
-  // render() {
-  //   return (
-  //     <View style={styles.container}>
-  //       <Text style={styles.welcome}>
-  //         Welcome to the Recs Page!
-  //       </Text>
-  //     </View>
-  //   );
-  // }
+  getRecs() {
+    return fetch(`${this.props.apiRoot}/api/recs/${this.props.userId}/${this.state.lat}/${this.state.long}`)
+    .then((result) => {
+      this.setState({
+        recs: ds.cloneWithRowsAndSections({recList: this.state.fakeData}) //need to fix
+      });
+      // console.log(this.state.recs)
+    })
+    .catch((err) => console.error(err));
+  }
 
   componentWillMount() {
     navigator.geolocation.getCurrentPosition((position) => {
-      var initialPosition = position;
-      this.setState({initialPosition: initialPosition});
-      // alert(JSON.stringify(this.state.initialPosition))
-      // console.log("coords", this.state.initialPosition.coords)
-      // console.log("posit", this.state.initialPosition)
-    },
-    (error) => alert('Please enable location services.'),
-    {enableHighAccurracy: true, timeout: 20000, maxinumAge: 1000}
-    );
+      // console.log(position)
+      var longitude = position.coords.longitude;
+      var latitude = position.coords.latitude;
+      this.setState({long: longitude});
+      this.setState({lat: latitude});
+      this.getRecs();
+    }, (error) => {
+      alert('Please enable location services.');
+    }, {
+      enableHighAccurracy: true, timeout: 20000, maxinumAge: 1000
+    });
   }
 
-  render() {  
+  render() {
+    if (this.state.recs === undefined || this.state.recs.getRowCount() === 0) {
+      return (
+        <View style={styles.container}>
+          <Text style={styles.welcome}>Recs page</Text>
+          <Text style={styles.welcome}>Long: {this.state.long ? this.state.long : 'Please enable location services'}</Text>
+          <Text style={styles.welcome}>Lat: {this.state.lat ? this.state.lat : 'Please enable location services'}</Text>
+          <Text>Loading...</Text>
+        </View>
+      );
+    } else {
+      console.log(this.state.recs);
+      return (
+        <View style={styles.container}>
+          <Text style={styles.welcome}>Recs page</Text>
+          <Text style={styles.welcome}>Long: {this.state.long ? this.state.long : 'Please enable location services'}</Text>
+          <Text style={styles.welcome}>Lat: {this.state.lat ? this.state.lat : 'Please enable location services'}</Text>
+          <ListView dataSource={this.state.recs} renderRow={(rec) => this.recEntry(rec)}/>
+        </View>
+      );
+      // return (
+      //   <View style={styles.container}>
+      //     <Text style={styles.welcome}>Recs page</Text>
+      //     <Text style={styles.welcome}>Long: {this.state.long ? this.state.long : 'Please enable location services'}</Text>
+      //     <Text style={styles.welcome}>Lat: {this.state.lat ? this.state.lat : 'Please enable location services'}</Text>
+      //     <Text>Loading...</Text>
+      //   </View>
+      // )
+    }
+  }
+
+  recEntry(rec) {
+    console.log(rec);
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Recs page</Text>
-        <Text style={styles.welcome}>Long: {this.state.initialPosition ? this.state.initialPosition.coords.longitude : 'Please enable location services'}</Text>
-        <Text style={styles.welcome}>Lat: {this.state.initialPosition ? this.state.initialPosition.coords.latitude : 'Please enable location services'}</Text>
+      <View style={styles.card} >
+        <Image source={{uri: rec.url}}
+        resizeMode='contain' style={{width: 350, height: 35 * 10}} />
+        <View style={{width: 350, height: 70, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
+          <View style={{flexDirection: 'row', margin: 15, marginTop: 25, alignItems: 'center'}} >
+          <Text style={{fontSize: 12, fontWeight: '400', textAlign: 'center', color: '#444'}}>{rec.name}</Text>
+          </View>
+        </View>
       </View>
     );
   }
