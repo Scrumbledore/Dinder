fa5a817a99c5b131d08cadfd92f27b21b648e1da
Ryan Busby <BusbyActual@gmail.com>
2016-10-01 18:46:24 -0700

    Pushing up what I have so far to get another pair of eyes on the issue

--- Machine Learning/queryStuff.js
+++ Machine Learning/queryStuff.js
@@ -1,17 +1,17 @@
 var synaptic = require('synaptic');
-var s = require('sequelize');
 var Architect = synaptic.Architect;
-var Network;
+var Network; //defining after we get data for training
 var Category = require('../server/database/models/category.js');
-var userPhotos = require('../server/database/models/userPhotos.js');
 var photo = require('../server/database/models/photo.js');
 var place = require('../server/database/models/place.js');
 var User = require('../server/database/models/user.js');
-var handler = require('../server/handlers/userHandler.js');
+
+//for testing locally
 var connection = require('../server/database/database.js');
 require('../server/database/joins.js')(connection);
-var trainingData = [
-];
+
+var beforeTraining = [];
+var trainingData = [];
 
 var trainingOptions = {
   rate: .15,
@@ -20,54 +20,75 @@ var trainingOptions = {
 };
 
 
-// // Make training data array part 1.
-Category.aggregate('name', 'DISTINCT', { plain: false })
+User.findOne({ where: {id:21}})  // fixMe take in dynamic userID
 .then(function(data) {
-  return data.map(function(DISTINCT) {
-    return DISTINCT.DISTINCT;
-  });
+  //console.log(data);
+  return data.getPhotos();
 })
-.then(function(category){
-  category.forEach(function(data){
-    trainingData.push(makeTraining(category.length));
-  });
-  Network = new Architect.Perceptron(category.length, Math.floor(category.length * .2),category.length);
-  return category;
-})
-.then(function(train){
-  Network.trainer.train(trainingData,trainingOptions);
-  //log margin of error
-  return train;
-})
-.then(function(data){
-  var testObject = makeTraining(data.length);
-  var returnItems = [];
-  console.log(Network.activate(testObject.input));
-  for(var i = 0; i < testObject.input.length; i++){
-    if( testObject.input[i] > 0) {
-      returnItems.push({
-        category: data[i],
-        chance: testObject.input[i]
-      });
+.then(function(photos) {
+  var training = [];
+  photos.map(function(photo) {
+    var tueple = [[], 0, 0];
+    if (photo.UserPhotos.dataValues.like === true) {
+      if (photo.UserPhotos.dataValues.favorite === true) {
+        tueple[1] = 1;
+      } else {
+        tueple[1] = .6;
+      }
+    } else if (photo.UserPhotos.dataValues.favorite === true ) {
+      tueple[1] = .8;
     }
-  }
-  console.log(returnItems);
+    tueple[2] = photo.PlaceId;
+    training.push(tueple);
+  });
+  training.map(function(item) {
+    Category.findAll({ attributes: ['name'], where: {PlaceId: 26} })
+    .then(function(data) {
+      console.log(data);
+    });
+  });
+  console.log(training);
+  return training;
+})
+.then(function(categories) {
+  beforeTraining = categories;
 });
 
-// User.findOne({ where: {id:3}})
-// .then(function(data) {
-//   return data.getPhotos();
-// })
-// .then(function(photos) {
-//   photos.forEach(function(photo){
-//     console.log(photo.UserPhotos,'$$$$$$$$$$$$$$$$' +photo.PlaceId);
-//   })
 
+// // old training data keeping until new feature works
+
+// Category.aggregate('name', 'DISTINCT', { plain: false })
+// .then(function(data) {
+//   return data.map(function(DISTINCT) {
+//     return DISTINCT.DISTINCT;
+//   });
 // })
-// Photo.findOne({})
-// .then(function(photo){
-//   console.log(photo);
+// .then(function(category){
+//   category.forEach(function(data){
+//     trainingData.push(makeTraining(category.length));
+//   });
+//   Network = new Architect.Perceptron(category.length, Math.floor(category.length * .2),category.length);
+//   return category;
 // })
+// .then(function(train){
+//   Network.trainer.train(trainingData,trainingOptions);
+//   //log margin of error
+//   return train;
+// })
+// .then(function(data){
+//   var testObject = makeTraining(data.length);
+//   var returnItems = [];
+//   console.log(Network.activate(testObject.input));
+//   for(var i = 0; i < testObject.input.length; i++){
+//     if( testObject.input[i] > 0) {
+//       returnItems.push({
+//         category: data[i],
+//         chance: testObject.input[i]
+//       });
+//     }
+//   }
+//   console.log(returnItems);
+// });
 
 var makeTraining = function(n) {
   var newArray = Array.apply(null, Array(n)).map(Number.prototype.valueOf,0);
