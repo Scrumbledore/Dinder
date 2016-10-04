cbd7ee06118214c67bc28c5316d52df2372b152d
Ryan Busby <BusbyActual@gmail.com>
2016-10-03 20:36:28 -0700

    Tests implemented

--- spec/components/index.js
+++ spec/components/index.js
@@ -1,43 +1,31 @@
 import React from 'react';
 import { expect } from 'chai';
-import { mount, shallow } from 'enzyme';
+import { shallow, mount, render } from 'enzyme';
 import Menu from '../../app/components/Menu'
 
 
-import jsdom from 'jsdom'
-const doc = jsdom.jsdom('<!doctype html><html><body></body></html>')
-global.document = doc
-global.window = doc.defaultView
+// import jsdom from 'jsdom'
+// const doc = jsdom.jsdom('<!doctype html><html><body></body></html>')
+// global.document = doc
+// global.window = doc.defaultView
 
-// test('Visit component', (assert) => {
+describe('<Menu />', () => {
 
-//   const testVisit = () => {
-//     console.log('just visiting...')
-//   }
+  it('Menu component should exist', () => {
+    let component = shallow(<Menu props='stuff'/>);
+    //const wrapper = mount(<Menu />);
+    expect(component).to.exist;
 
-//   const component = shallow(<Visit />)
-//   const wrapper = mount(<Visit visited={testVisit} />)
-
-//   assert.equal(
-//     component.find('span').text(), '', 'the visit component has no text'
-//   )
-
-//   assert.pass(
-//     expect(component.find('span').text(), '')
-//   )
-
-//   assert.equal(
-//     wrapper.props().visited, testVisit, 'the visit component has a visited prop'
-//   )
-
-//   assert.end()
-// });
-describe('<menu />', (assert) => {
-
-  it('calls componentDidMount', () => {
-    const component = shallow(<Menu />);
-    const wrapper = mount(<Menu />);
-    expect(Menu.prototype.componentDidMount.calledOnce).to.equal(true);
   });
 
+
+  it('It should render contents ', () => {
+    let component = shallow(<Menu swag='stuff'/>);
+    // const wrapper = mount(<Menu />);
+
+    let namePar = component.childAt(0);
+    //console.log(namePar.node)
+    expect(namePar.node.props.style.fontSize).to.equal(24);
+    expect(namePar.node.props.children).to.equal('Welcome to the Menu Page!');
+  });
 });
