d705c040963d6f1a0b50640d601515f57ca0a38b
Ryan Busby <BusbyActual@gmail.com>
2016-10-07 15:58:46 -0700

    Setup Redux and base routes. SignIn/SignUp/Food currently working.

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { AppRegistry, Text, View, Platform, AsyncStorage } from 'react-native';
-import { Scene, Router, Actions, TabBar, Navigator } from 'react-native-router-flux';
+import { Scene, TabBar, Navigator } from 'react-native-router-flux';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import SignIn from './components/SignIn';
@@ -13,8 +13,14 @@ import RNCamera from './components/Camera';
 import Photos from './components/Photos';
 import CameraRoll from './components/CameraRoll';
 
+import {Router, routerReducer, Route, Container, Animations, Actions, Schema} from 'react-native-redux-router';
 
 var config = require('../config.js');
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+
+let store = createStore(combineReducers({routerReducer}));
 
 // for tab menu below, not used elsewhere
 class TabIcon extends Component {
@@ -49,86 +55,57 @@ export default class Dinder extends Component {
   }
 
   componentWillMount() {
-    AsyncStorage.getItem('jwt')
-    .then((value) => {
-      if (value) {
-        Actions.tabbar();
-      }
-    }).done();
+    // AsyncStorage.getItem('jwt')
+    // .then((value) => {
+    //   if (value) {
+    //     Actions.tabbar();
+    //   }
+    // }).done();
   }
 
   render() {
     return (
-      <Router hideNavBar={true} >
-        <Scene key='root' hideNavBar={true}>
-          <Scene
-            key='signin'
-            component={SignIn}
-            icon={TabIcon}
+        <View style={{flex:1}}>
+            <View style={{position:'absolute',left:0,right:0,top:0,bottom:0,backgroundColor:'#F5FCFF'}}/>
+            <Provider store={store}>
+            <Router>
+                <Schema name="modal" sceneConfig={Animations.FlatFloatFromBottom} />
+                <Schema name="default" sceneConfig={Animations.FlatFloatFromRight} />
+                <Schema name="withoutAnimation"/>
+                <Schema name="tab" navbar={TabIcon}/>
+
+
+                <Route name="signin" component={SignIn} initial={true} schema="modal"  icon={TabIcon}
             apiRoot={this.state.apiRoot}
-            title='Sign In' />
-          <Scene
-            key='signup'
-            component={SignUp}
-            icon={TabIcon}
-            apiRoot={this.state.apiRoot}
-            title='Sign Up' />
-          <Scene
-            type='replace'
-            key='tabbar'
-            tabs={true}>
-            <Scene
-              key='camera'
-              component={RNCamera}
-              apiRoot={this.state.apiRoot}
-              userId={this.state.userId} />
-            <Scene
-              key='cameraroll'
-              component={CameraRoll}
-              apiRoot={this.state.apiRoot}
-              userId={this.state.userId} />
-            <Scene
-             img='camera'
-             key='photos'
-              component={Photos}
-              icon={TabIcon}
-              title='Photos'
-              apiRoot={this.state.apiRoot}
-              userId={this.state.userId} />
-            <Scene
-              img='star-border'
-              key='favorites'
-              component={Favorites}
-              icon={TabIcon}
-              title='Favorites'
-              apiRoot={this.state.apiRoot}
-              userId={this.state.userId}/>
-            <Scene
-              img='local-pizza'
+            title='Sign In' key='signin'/>
+                <Route name="signup" component={SignUp} schema="tab"/>
+
+
+            <Route name="food"  img='local-pizza'
               key='food'
-              initial={true}
               component={Food}
               icon={TabIcon}
               title='Food'
               apiRoot={this.state.apiRoot}
-              userId={this.state.userId}/>
-            <Scene
-              img='assistant'
-              key='recs'
-              component={Recs}
-              icon={TabIcon}
-              apiRoot={this.state.apiRoot}
               userId={this.state.userId}
-              title='Recs' />
-            <Scene
-              img='menu'
-              key='menu'
-              component={Menu}
-              icon={TabIcon}
-              title='Menu' />
-          </Scene>
-        </Scene>
-      </Router>
+              schema={"tab"}/>
+
+            </Router>
+            </Provider>
+
+        </View>
     );
   }
 }
+
+class Example extends React.Component {
+    render() {
+        return (
+            <Provider store={store}>
+                {() => <Dinder />}
+            </Provider>
+        );
+    }
+}
+
+AppRegistry.registerComponent('Example', () => Example);
