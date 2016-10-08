508a17a6510edf47944fafe1ae2b9d77ff176e84
Ryan Busby <BusbyActual@gmail.com>
2016-10-07 21:51:01 -0700

    Updated Nav to work and hacky styles

--- app/components/Photos.js
+++ app/components/Photos.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-import { Actions } from 'react-native-router-flux';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { Actions } from 'react-native-redux-router';
 import { Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 
 import styles from '../styles/styles.js';
@@ -13,11 +14,11 @@ export default class Photos extends Component {
   }
 
   getCamera() {
-    Actions.camera();
+    Actions.Camera;
   }
 
   getCameraRoll() {
-    Actions.cameraroll();
+    Actions.Cameraroll;
   }
 
   render() {
@@ -26,16 +27,31 @@ export default class Photos extends Component {
         <Text style={styles.welcome}>
           Add your photos!
         </Text>
-          <Button
-            onPress={this.getCamera.bind(this)}
-            buttonStyle={styles.buttonBlue}
-            title='Take New Picture' />
-          <Button
-            onPress={this.getCameraRoll.bind(this)}
-            buttonStyle={styles.buttonBlue}
-            title='Get Photo From Library' />
-
-        
+        <Button
+          onPress={Actions.Camera}
+          buttonStyle={styles.buttonBlue}
+          title='Take New Picture' />
+        <Button
+          onPress={Actions.CameraRoll}
+          buttonStyle={styles.buttonBlue}
+          title='Get Photo From Library' />
+        <View style={{flexDirection: 'row', top: 170}}>
+          <TouchableOpacity style={styles.foodNav} onPress = {Actions.Photos}>
+            <Icon name='camera' size={50} color={this.props.selected ? 'steelblue' : 'black' } />
+          </TouchableOpacity>
+           <TouchableOpacity style={styles.foodNav} onPress = {Actions.Favorites}>
+            <Icon name='star-border' size={50} color={this.props.selected ? 'steelblue' : 'black' } />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.foodNav} onPress = {Actions.Food}>
+            <Icon name='local-pizza' size={50} color={this.props.selected ? 'steelblue' : 'black' } />
+          </TouchableOpacity>
+             <TouchableOpacity style={styles.foodNav} onPress = {Actions.Recs}>
+            <Icon name='assistant' size={50} color={this.props.selected ? 'steelblue' : 'black' } />
+          </TouchableOpacity>
+             <TouchableOpacity style={styles.foodNav} onPress = {Actions.Menu}>
+            <Icon name='menu' size={50} color={this.props.selected ? 'steelblue' : 'black' } />
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
